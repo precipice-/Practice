dacb24367bc3637f49b79061d145f612c30d53b7
precipice- <configure62@gmail.com>
2022-08-03 02:41:12 +0900

    Initial commit

---
+++ day1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    std::cout << "hi" << std::endl
+              << "my name is "
+              << "Psi" << std::endl;
+
+    return 0;
+}
